63d90af46897b53a436184d1c5db3f9759aaf62f
ISHIDA, Megumi <34me4108@gmail.com>
2023-02-04 09:16:53 +0900

    RData

--- test/devtool.R
+++ test/devtool.R
@@ -5,6 +5,7 @@ devtools::install_github("ishidamgm/kuraiyamaK123")
 devtools::install_github("ishidamgm/nenrin")
 library(kuraiyamaK123)
 help(package="kuraiyamaK123")
+data("kuraiyamaK123")
 d
 d2
 edit(d3)
